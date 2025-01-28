ad52138ad429d849e2e94df44a8be16f763b0a8e
CamiloScript <camilo.gonzalezja@hotmail.com>
2025-01-28 16:46:09 -0300

    enlace base de datos moango db

--- bd/main.go
+++ bd/main.go
@@ -1,4 +1,4 @@
-package bd
+package main
 
 import (
 	"context"
@@ -7,11 +7,13 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
   )
+
+
   func main() {
 
 	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI("mongodb+srv://CamiloDB:<db_password>@entornopruebabd.snitb.mongodb.net/?retryWrites=true&w=majority&appName=EntornoPruebaBD").SetServerAPIOptions(serverAPI)
+	opts := options.Client().ApplyURI("mongodb+srv://CamiloDB:209451@entornopruebabd.snitb.mongodb.net/?retryWrites=true&w=majority&appName=EntornoPruebaBD").SetServerAPIOptions(serverAPI)
 
 	// Create a new client and connect to the server
 	client, err := mongo.Connect(context.TODO(), opts)
@@ -23,10 +25,12 @@ import (
 		panic(err)
 	  }
 	}()
-	
+
 	// Send a ping to confirm a successful connection
 	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
 	  panic(err)
 	}
 	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
   }
+
+
