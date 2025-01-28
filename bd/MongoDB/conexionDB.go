d26d47834e3c7e90df55810285bf9648a113cc5d
CamiloScript <camilo.gonzalezja@hotmail.com>
2025-01-28 20:19:24 -0300

    instalacion de DOTENV para variables de entorno y seguridad de datos.

--- bd/MongoDB/conexionDB.go
+++ bd/MongoDB/conexionDB.go
@@ -3,37 +3,55 @@ package MongoDB
 import (
 	"context"
 	"fmt"
+	"os"
+
+	"github.com/rs/zerolog/log"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-  )
+	"github.com/joho/godotenv"
+)
 
+// ConexionDB establece la conexión con MongoDB utilizando URI desde el archivo .env.
+func ConexionDB() (*mongo.Client, error) {
+	// Cargar variables del archivo .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Error().Err(err).Msg("Error al cargar el archivo .env")
+		return nil, fmt.Errorf("Error al cargar el archivo .env: %v", err)
+	}
 
-  func ConexionDB() {
+	// Obtener URI de la base de datos desde la variable de entorno
+	uri := os.Getenv("MONGO_URI")
+	if uri == "" {
+		log.Error().Msg("URI de MongoDB no encontrada en las variables de entorno")
+		return nil, fmt.Errorf("URI de MongoDB no encontrada en las variables de entorno")
+	}
 
-	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
+	// Opciones de conexión
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI("mongodb+srv://CamiloDB:209451@entornopruebabd.snitb.mongodb.net/?retryWrites=true&w=majority&appName=EntornoPruebaBD").SetServerAPIOptions(serverAPI)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
 
-	// Create a new client and connect to the server
+	// Crear cliente y conectarse a la base de datos
 	client, err := mongo.Connect(context.TODO(), opts)
 	if err != nil {
-	  panic(err)
+		log.Error().Err(err).Msg("Error al conectar con la base de datos MongoDB")
+		return nil, fmt.Errorf("Error al conectar con MongoDB: %v", err)
 	}
-	defer func() {
-	  if err = client.Disconnect(context.TODO()); err != nil {
-		panic(err)
-	  }
-	}()
 
-	// Send a ping to confirm a successful connection
-	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
-	  panic(err)
+	// Verificar conexión mediante un ping
+	err = client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err()
+	if err != nil {
+		log.Error().Err(err).Msg("Error al hacer ping a MongoDB")
+		return nil, fmt.Errorf("Error al hacer ping a MongoDB: %v", err)
 	}
-	fmt.Println("Conexión exitosa a la base de datos MongoDB!")
 
+	// Registro de conexión exitosa
+	log.Info().Msg("Conexión exitosa a la base de datos MongoDB")
 
-  }
+	return client, nil
+}
+
 
 
 
